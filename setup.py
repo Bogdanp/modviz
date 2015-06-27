c187b23144ca844043af7bff07dd5ef35436828f
Bogdan Popa <popa.bogdanp@gmail.com>
2015-06-27 21:49:59 +0300

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="modviz",
-    version="0.1.2",
+    version="0.1.3",
     description="Modviz lets you visualize your module dependencies.",
     long_description="https://github.com/Bogdanp/modviz",
     packages=["modviz"],
